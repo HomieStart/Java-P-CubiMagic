4177da92ab211f79ebe4da0c3eed84537b56ebff
F-Homie <tr.x.homie@gmail.com>
2016-11-10 03:49:18 -0300

    Update Files.java

--- FILE-IO/core/File/Files.java
+++ FILE-IO/core/File/Files.java
@@ -13,12 +13,19 @@ package core.File;
 public abstract class Files {
     protected long TIMER;
     protected int BUFFER_SIZE;
-    
+    /**
+     * [Files]
+     * @param KiloBytes define size of buffer
+     */
     public Files(int KiloBytes){
         this.TIMER = 0;
         this.BUFFER_SIZE = 1024 * KiloBytes;
     }
     
+    /**
+     * [Get total time elapsed when finalize a action]
+     * @return time elapse when execute any action 
+     */
     protected long getTimeElapsed(){
         return this.TIMER;
     }
