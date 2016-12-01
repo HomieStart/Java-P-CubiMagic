ee92a4ee7f08f3bf6bbb5f845b7225a5543e6417
F-Homie <tr.x.homie@gmail.com>
2016-12-01 15:31:54 -0300

    Laberinto v0.2

---
+++ LaberintoNumeral/src/com/functions/UConfig.java
@@ -0,0 +1,16 @@
+package com.functions;
+/** 
+ * @author TR-X Homie
+ * @project LaberintoNumeral Santa-Lucia
+ * @version 0.2
+ * @license Creative Commons
+ */
+public final class UConfig {
+    public static boolean MENSAJES_CONSOLA = true; 
+    public static boolean USAR_INTERFAZ_GRAFICA = true;
+    public static boolean MOSTRAR_AVISO_DE_PREPROCESO = true;
+    public static boolean PERMITIR_TODOS_LOS_POSIBLES_CAMINOS = false;
+    public static boolean LIMITAR = false;
+    public static int DIMENSIONES_POR_DEFECTO[] = new int[]{4,4};
+    public static int TOTAL_DE_LIMITE = 50;
+}
